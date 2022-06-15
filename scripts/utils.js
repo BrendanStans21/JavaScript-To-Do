9391df706cd3ce337aa8453ca676c6f0d3da8a0a
BrendanStans21 <brendanstansfield21@gmail.com>
2022-06-15 16:27:58 -0600

    added more html and css styling, implenting save, clear, and delete task button in the js file

---
+++ scripts/utils.js
@@ -0,0 +1,22 @@
+const saveButton = document.getElementById("save-btn");
+const clearButton = document.getElementById("clear-btn");
+const deleteButton = document.getElementById("delete-tasks-btn");
+
+saveButton.addEventListener('click', function() {
+    let newTask = document.getElementById("new-task").value;
+    let taskList = document.getElementById("tasks");
+
+    document.getElementById("list-container").style.visibility = "visible";
+
+    taskList.innerHTML += "<li>" + newTask + "<input class='done-btn' type='button' value='Done'/></li>";
+    document.getElementById("new-task").value = "";
+});
+
+clearButton.addEventListener('click', function() {
+    document.getElementById("new-task").value = "";
+});
+
+deleteButton.addEventListener('click', function() {
+    document.getElementById("tasks").innerHTML = "";
+    document.getElementById("list-container").style.visibility = "hidden";
+}); 
